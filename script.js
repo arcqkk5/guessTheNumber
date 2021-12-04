6ab7fb446fe9f8511de6ee69bf660983cfeee22e
arcq-k5 <arcqk5@yandex.ru>
2021-12-04 15:41:12 +0300

    add logic

--- script.js
+++ script.js
@@ -8,8 +8,8 @@
 
 //events
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.question').textContent = secretNumber;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
@@ -26,6 +26,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.guess-message').textContent = 'Вы проиграли(';
+      document.querySelector('.score').textContent = 0;
     }
   } else if (guessingNumber < secretNumber) {
     if (score > 1) {
@@ -34,8 +35,28 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.guess-message').textContent = 'Вы проиграли(';
+      document.querySelector('.score').textContent = 0;
     }
   } else if (guessingNumber == secretNumber) {
+    if (highScore < score) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
     document.querySelector('.guess-message').textContent = 'Верно! Молодец!';
+    document.querySelector('.question').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
+    document.querySelector('.question').style.width = '50rem';
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
+  document.querySelector('.question').textContent = '???';
+  document.querySelector('.question').style.width = '25 rem';
+  document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number-input').value = '';
 });
